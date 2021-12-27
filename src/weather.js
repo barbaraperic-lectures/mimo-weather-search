39be47c90f74fcabce19e952abfd4d757d52d1f4
Barbara Peric <b.peric10@gmail.com>
2021-12-27 12:31:32 +0100

    FEAT: moment js

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,9 @@
+import { initMoment } from "./helper/init_moment";
+
 const temperature = document.querySelector(".temperature");
 const weatherIcon = document.getElementById("weather-icon");
 const cityName = document.querySelector(".city");
+const time = document.querySelector(".time");
 
 const fetchWeatherData = (city) => {
 	fetch(
@@ -11,13 +14,16 @@ const fetchWeatherData = (city) => {
 };
 
 const insertWeatherData = (data) => {
+	console.log(data);
 	const city = data.name;
 	const temp = data.main.temp;
 	const iconcode = data.weather[0].icon;
 	const description = data.weather[0].description;
+	const timeText = initMoment(data.timezone);
 	weatherIcon.src = `http://openweathermap.org/img/w/${iconcode}.png`;
 	cityName.innerText = `Weather in ${city}`;
 	temperature.innerText = `Temperature is ${temp}ºC, ${description}`;
+	time.innerText = timeText;
 };
 
 export { fetchWeatherData };
